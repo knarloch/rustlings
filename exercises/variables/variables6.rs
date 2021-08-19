c375502d9bb76dd9685d780609fdb2a55f8e11da
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2021-08-20 00:28:52 +0200

    Did some exercises

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,9 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const fn x() ->i32 { return 2;}
+const NUMBER :i32 = x();
+const y :i32 = x() *2;
 fn main() {
     println!("Number {}", NUMBER);
 }
