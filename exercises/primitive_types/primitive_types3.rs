54bf70fb5526c5af10ee2d0f186de67e3f17e910
Krzysztof Narloch <knarloch@inpost.pl>
2021-10-10 19:13:08 +0200

    Did way more.

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,12 +2,11 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // let a : [char; 100] = ['\u{1F63B}'; 100];
+    let ą: [char; 100] = ['ą'; 100];
 
-    if a.len() >= 100 {
+    if ą.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
